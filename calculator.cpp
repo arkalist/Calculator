df09d01e5ff37a28a6da634022c52d5b3e8c9a47
arkalist <72329668+arkalist@users.noreply.github.com>
2020-10-14 02:23:56 +0800

    Add files via upload

---
+++ calculator.cpp
@@ -0,0 +1,41 @@
+# include <iostream>
+using namespace std;
+
+int main()
+{
+    char op;
+    float num1, num2;
+
+    cout << "Enter operator either + or - or * or /: ";
+    cin >> op;
+
+    cout << "Enter 1st number: ";
+    cin >> num1;
+	cout << "Enter 1st number: ";
+	cin >> num2;
+    switch(op)
+    {
+        case '+':
+            cout <<"Total = "<< num1+num2;
+            break;
+
+        case '-':
+            cout <<"Total = "<< num1-num2;
+            break;
+
+        case '*':
+            cout <<"Total = "<< num1*num2;
+            break;
+
+        case '/':
+            cout <<"Total = "<< num1/num2;
+            break;
+
+        default:
+            // If the operator is other than +, -, * or /, error message is shown
+            cout << "Error! operator is not correct";
+            break;
+    }
+
+    return 0;
+}
